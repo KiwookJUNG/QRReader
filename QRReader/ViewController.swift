554c6dfef9baac1b4105bdc03c599a74d5500287
정기욱 <rldnr56@me.com>
2019-03-07 12:33:42 +0900

    captureOutput 메소드는
    매개변수로 들어온 메타데이터 오브젝트가 있으면
    
    그 데이터를 AVMetadataMachineReadableCodeObject로 타입 캐스팅해주고
    
    그 데이터가 qr코드이면 stringValue로 바꿔서 stringURL에 넣어준다.

--- QRReader/ViewController.swift
+++ QRReader/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate    {
 
+    @IBOutlet var videoPreview: UIView!
+    
+    var stringURL = String()
+    
+    enum error: Error {
+        case noCameraAvailable
+        case videoInputInitFail
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            
+        } catch {
+             print("Failed to scan the QR code.")
+        }
+    }
+    
+    func captureOutput (_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects : [Any], from connection : AVCaptureConnection!) {
+        
+        if metadataObjects.count > 0 { // 매개변수로 들어온 데이터가 있으면
+            let machineReaderbleCode = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+            // metadataObjects[0]을 AVMetadataMachineReadableCodeObject로 타입캐스팅 해준다.
+            if machineReaderbleCode.type == AVMetadataObject.ObjectType.qr {
+                // 만약 카메라가 읽은 데이터가 qr코드 타입이면 string value로 바꿔 준뒤 stringURL에 넣어준다.
+                stringURL = machineReaderbleCode.stringValue!
+            }
+        }
+        
     }
 
 
