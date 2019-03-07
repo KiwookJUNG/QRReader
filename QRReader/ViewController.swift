9eff224795779e35d9ecc309d8816774baf6035a
정기욱 <rldnr56@me.com>
2019-03-07 12:12:56 +0900

    Initial Commit

---
+++ QRReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRReader
+//
+//  Created by 정기욱 on 07/03/2019.
+//  Copyright © 2019 Kiwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
